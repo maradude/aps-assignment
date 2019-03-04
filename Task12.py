bdba36326cc853b86569eefda4462b3d35daa59f
Martti <martti@aukia.com>
2019-03-04 01:35:48 +0000

    add Task1.2 implementation

---
+++ Task12.py
@@ -0,0 +1,44 @@
+class SortedArray:
+
+    def __init__(self, *args):
+        self._values = list(sorted(args))
+        self._len = len(self._values)
+
+    def __len__(self):
+        return self._len
+
+    def get_range(self, lbound, ubound):
+        # do 2 log n searches, get elements from between
+        min_i = SortedArray._findStart(self._values, lbound, 0, len(self))
+        max_i = SortedArray._findEnd(self._values, ubound, 0, len(self))
+        return self._values[min_i:max_i]
+
+    @staticmethod
+    def _findStart(arr, target, lbound, ubound):
+        # it's the left most k algorithm, lifted from slides pseudocode
+        if ubound < lbound:
+            return lbound
+        mid_point = (lbound+ubound) // 2
+        if arr[mid_point] < target:
+            return SortedArray._findStart(arr, target, mid_point+1, ubound)
+        else:
+            return SortedArray._findStart(arr, target, lbound, mid_point-1)
+
+    @staticmethod
+    def _findEnd(arr, target, lbound, ubound):
+        # find the index of the last occurance of k in the array
+        if lbound > ubound:
+            return lbound
+        mid_point = (lbound+ubound) // 2
+        if arr[mid_point] > target:
+            return SortedArray._findEnd(arr, target, lbound, mid_point-1)
+        else:
+            return SortedArray._findEnd(arr, target, mid_point+1, ubound)
+
+
+if __name__ == '__main__':
+    from sanitize_input import get_input
+    test_object = get_input()
+    nlist = SortedArray(*test_object.elements)
+    for lower, upper in test_object.ranges:
+        print(' '.join(map(str, nlist.get_range(lower, upper))))
