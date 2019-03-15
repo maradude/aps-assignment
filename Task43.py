0b8ef800ec708cb5351d30a96aa8cb0f19cb0bf8
Martti <martti@aukia.com>
2019-03-15 17:37:55 +0000

    clean 4.3 move tests to own directory

--- Task43.py
+++ Task43.py
@@ -18,31 +18,11 @@ class KDTree:
 
     @staticmethod
     def BuildKDTree(points, current_depth):
-        """
-        Code from wikipedia:
-        Wikipedia contributors. (2019, March 8). K-d tree. In Wikipedia,
-        The Free Encyclopedia. Retrieved 18:06, March 10, 2019,
-        from https://en.wikipedia.org/w/index.php?title=K-d_tree&oldid=886834497
-
-        just cuz it's better than my attempt
-        """
         if len(points) == 1:
             return KDTree._Node(points[0])
         axis = current_depth % len(points[0])
-        # axis = current_depth % k
         points.sort(key=itemgetter(axis))
         medianPoint = points[(len(points)-1) // 2][axis]  # choose median
-        # Create node and construct subtrees
-        # return KDTree._Node(
-            # data=points[median],
-            # left=KDTree.BuildKDTree(points[:median +1], current_depth + 1),
-            # right=KDTree.BuildKDTree(points[median + 1:], current_depth + 1)
-        # )
-
-        # medianPoint = KDTree.get_median_point(points, axis)
-
-        # left_points = [p for p in points if p[axis] <= medianPoint]
-        # right_points = [p for p in points if p[axis] > medianPoint]
         left_points = points[:((len(points)-1) // 2)+1]
         right_points = points[((len(points)-1) // 2)+1:]
 
