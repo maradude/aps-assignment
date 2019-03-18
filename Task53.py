2d81341a4b2fa61b7c49e4dafe4ed8e33194f63a
Martti <martti@aukia.com>
2019-03-18 20:45:15 +0000

    add benchmarking code

---
+++ Task53.py
@@ -0,0 +1,83 @@
+from Task21 import RangeTree
+from copy import deepcopy
+
+
+def intersect(minMax1, minMax2):
+    # each dimensions lowest value, highest value
+    for d in range(len(minMax1)):
+        if not (minMax2[d][0] <= minMax1[d][0] <= minMax2[d][1] or
+                minMax2[d][0] <= minMax1[d][1] <= minMax2[d][1] or
+                minMax1[d][0] <= minMax2[d][0] <= minMax1[d][1] or
+                minMax1[d][0] <= minMax2[d][1] <= minMax1[d][1]):
+            return False
+    return True
+
+
+def is_point_in_range(point, region):
+    def check(d): return region[d][0] <= point[d] <= region[d][1]
+    return all(map(check, range(len(region))))
+
+
+def contained(super_set, sub_set):
+    def check(d, region, point): return (region[d][0] <= point[d][0]
+                                         <= point[d][1] <= region[d][1])
+    return all(check(d, super_set, sub_set) for d in range(len(sub_set)))
+
+
+def get_all_values(array, node):
+    return RangeTree.get_tree_values(array, node)
+
+
+def get_range(node, region):
+    bounds = [[float('-inf'), float('inf')] for _ in range(len(region))]
+    return SearchKDTreeNoContained(node, region, current_region=bounds, current_depth=0)
+
+
+def new_region(node, region, dimension, bound):
+    region[dimension][bound] = node.data
+    return region
+
+
+def SearchKDTreeNoContained(node, target_region, current_region, current_depth):
+    # TODO: swap extends with appends and then flatMap on return
+    # if node.data in [(10, 4), (23, 6), (30, 10)]:
+    # TODO: replace hardcopy() with something more efficient
+    reported_nodes = []
+    axis = current_depth % len(target_region)
+
+    if node.is_leaf():
+        if is_point_in_range(node.data, target_region):
+            reported_nodes.append(node.data)
+        return reported_nodes
+
+    left_region = new_region(node, deepcopy(current_region), axis, 1)
+
+    if intersect(left_region, target_region):
+        reported_nodes.extend(SearchKDTreeNoContained(
+            node.left, target_region, left_region, current_depth+1))
+
+    right_region = new_region(node, deepcopy(current_region), axis, 0)
+
+    if intersect(right_region, target_region):
+        reported_nodes.extend(SearchKDTreeNoContained(
+            node.right, target_region, right_region, current_depth+1))
+
+    return reported_nodes
+
+
+def main():
+    from Task43 import KDTree
+    # from print_binary_tree import printTree
+    tree = KDTree(test_object.elements)
+    # printTree(tree.root)
+    for test in test_object.ranges:
+        ranges_by_dimension = list(zip(test[0], test[1]))
+        a = get_range(tree.root, ranges_by_dimension)
+        b = [list(e) for e in (a)]
+        print(str(b).replace(',', '')[1:-1])
+
+
+if __name__ == '__main__':
+    from sanitize_input import get_input
+    test_object = get_input('kd')
+    main()
