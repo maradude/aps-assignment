ea4129836153066de787f429af805a0ec3ba246c
Martti <martti@aukia.com>
2019-03-19 02:30:54 +0000

    improve performance of 5.3 and 5.1 change region creation from deepcopy to shallow copy flattened region by use of row major indeces

--- Task53.py
+++ Task53.py
@@ -4,11 +4,11 @@ from copy import deepcopy
 
 def intersect(minMax1, minMax2):
     # each dimensions lowest value, highest value
-    for d in range(len(minMax1)):
-        if not (minMax2[d][0] <= minMax1[d][0] <= minMax2[d][1] or
-                minMax2[d][0] <= minMax1[d][1] <= minMax2[d][1] or
-                minMax1[d][0] <= minMax2[d][0] <= minMax1[d][1] or
-                minMax1[d][0] <= minMax2[d][1] <= minMax1[d][1]):
+    for d in range(len(minMax2)):
+        if not (minMax2[d][0] <= minMax1[val(0, d)] <= minMax2[d][1] or
+                minMax2[d][0] <= minMax1[val(1, d)] <= minMax2[d][1] or
+                minMax1[val(0, d)] <= minMax2[d][0] <= minMax1[val(1, d)] or
+                minMax1[val(0, d)] <= minMax2[d][1] <= minMax1[val(1, d)]):
             return False
     return True
 
@@ -19,26 +19,38 @@ def is_point_in_range(point, region):
 
 
 def contained(super_set, sub_set):
-    def check(d, region, point): return (region[d][0] <= point[d][0]
-                                         <= point[d][1] <= region[d][1])
-    return all(check(d, super_set, sub_set) for d in range(len(sub_set)))
+    def check(d, region, point): return (region[d][0] <= point[val(0, d)]
+                                         <= point[val(1, d)] <= region[d][1])
+    return all(check(d, super_set, sub_set) for d in range(len(super_set)))
 
 
 def get_all_values(array, node):
-    return RangeTree.get_tree_values(array, node)
+    RangeTree.get_tree_values(array, node)
 
 
 def get_range(node, region):
-    bounds = [[float('-inf'), float('inf')] for _ in range(len(region))]
-    return SearchKDTreeNoContained(node, region, current_region=bounds, current_depth=0)
+    bounds = [float('-inf'), float('inf')]*len(region)
+    return SearchKDTree(node, region, current_region=bounds, current_depth=0)
 
 
-def new_region(node, region, dimension, bound):
-    region[dimension][bound] = node.data
-    return region
+def val(bound, dimension):
+    # row-major indexing
+    return dimension*2 + bound
+
+# def new_region(node, region, dimension, bound):
+    # region[dimension][bound] = node.data
+    # return region
 
 
-def SearchKDTreeNoContained(node, target_region, current_region, current_depth):
+def new_region(value, region, dimension, bound):
+    # funky indexing so I don't have to make deepcopies
+    new_region = []
+    new_region.extend(region)
+    new_region[val(bound, dimension)] = value.data
+    return new_region
+
+
+def SearchKDTree(node, target_region, current_region, current_depth):
     # TODO: swap extends with appends and then flatMap on return
     # if node.data in [(10, 4), (23, 6), (30, 10)]:
     # TODO: replace hardcopy() with something more efficient
@@ -50,16 +62,20 @@ def SearchKDTreeNoContained(node, target_region, current_region, current_depth):
             reported_nodes.append(node.data)
         return reported_nodes
 
-    left_region = new_region(node, deepcopy(current_region), axis, 1)
+    left_region = new_region(node, current_region, axis, 1)
+    # if contained(target_region, left_region):
+        # get_all_values(reported_nodes, node.left)
 
     if intersect(left_region, target_region):
-        reported_nodes.extend(SearchKDTreeNoContained(
+        reported_nodes.extend(SearchKDTree(
             node.left, target_region, left_region, current_depth+1))
 
-    right_region = new_region(node, deepcopy(current_region), axis, 0)
+    right_region = new_region(node, current_region, axis, 0)
+    # if contained(target_region, right_region):
+        # get_all_values(reported_nodes, node.right)
 
     if intersect(right_region, target_region):
-        reported_nodes.extend(SearchKDTreeNoContained(
+        reported_nodes.extend(SearchKDTree(
             node.right, target_region, right_region, current_depth+1))
 
     return reported_nodes
